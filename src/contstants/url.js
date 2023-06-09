3b03b66cbab0706d5eeefec01b4324a222c06613
GazuaDev8417 <francadasilvaflamarion@gmail.com>
2023-06-09 19:34:18 -0300

    ajustes para deploy

--- src/contstants/url.js
+++ src/contstants/url.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client'
-export const url = 'https://chat-jcnn.onrender.com'
+export const url = 'https://chat-phi-woad.vercel.app'
 // export const url = 'http://localhost:3003'
 export const socket = io(url)
